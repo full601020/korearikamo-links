d4f1a4ced4d70ed0938ca9fe075bd7b6eda3913d
full601020 <326774749+full601020@users.noreply.github.com>
2026-09-12 17:30:47 +0900

    公開商品一覧を更新（1件）

--- products.js
+++ products.js
@@ -1,3 +1,11 @@
 // 商品発掘ダッシュボードから自動更新されます。
 // 公開先: https://full601020.github.io/korearikamo-links/
-window.PRODUCTS = [];
+window.PRODUCTS = [
+  {
+    "name": "SS期間限定★クーポンで5500円～★前開き スーツケース Sサイズ Mサイズキャリーバッグ フロントオープン 前ひらき スーツケースs USBポート付き ファスナー キャリーケース キャリーケース 機内持ち込み トップオープン 軽量 かわいい 大容量 1~3泊 4-7泊 44L 71L",
+    "category": "楽天市場",
+    "description": "レビュー★4.22（103件）・送料無料",
+    "price": "9,540円",
+    "url": "https://hb.afl.rakuten.co.jp/hgc/g00ts9zo.82zek221.g00ts9zo.82zel5d7/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fhomeycozy%2Fs801%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fhomeycozy%2Fi%2F10000821%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553"
+  }
+];

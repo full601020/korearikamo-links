a99039d4a49040997d29f68f40cda1c09d0bb35e
full601020 <326774749+full601020@users.noreply.github.com>
2026-09-12 17:30:38 +0900

    初回公開用の商品データを初期化

--- products.js
+++ products.js
@@ -1,15 +1,3 @@
-// 商品を追加するときは、下の配列へ同じ形式で追加します。
-// priceは「2,980円」のような表示用文字列です。
-// urlには必ず各ASPで発行されたアフィリエイトURLを入れてください。
-window.PRODUCTS = [
-  /*
-  {
-    name: "商品名",
-    category: "カテゴリ",
-    description: "商品の短い紹介文",
-    price: "2,980円",
-    image: "画像URL",
-    url: "アフィリエイトURL"
-  }
-  */
-];
+// 商品発掘ダッシュボードから自動更新されます。
+// 公開先: https://full601020.github.io/korearikamo-links/
+window.PRODUCTS = [];

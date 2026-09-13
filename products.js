4934975675c53e4198919975f54b4ea1406b700c
full601020 <326774749+full601020@users.noreply.github.com>
2026-09-13 17:42:40 +0900

    公開商品一覧を更新（11件）

--- products.js
+++ products.js
@@ -14,7 +14,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.71（2,291件）・送料無料",
     "price": "2,980円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00u05co.82zek0ac.g00u05co.82zel641/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fluckyhill%2Fnz-37tp%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fluckyhill%2Fi%2F10000105%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/luckyhill/cabinet/nz-37tp_b4.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "【楽天1位受賞】セミハードケース ガジェットケース トラベルポーチ 30cm 大型 大容量 収納ポーチ ガジェットポーチ 小物ポーチ ケーブル収納 持ち運びポーチ 小物収納 モバイルバッテリー 充電器 スマホ収納 旅行ポーチ 品 便利グッズ 出張",
@@ -22,7 +24,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.31（13件）・送料無料",
     "price": "2,780円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00pmmbo.82zek906.g00pmmbo.82zela6b/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fsanwadirect%2F200-bagin023bk%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fsanwadirect%2Fi%2F10098078%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/sanwadirect/cabinet/2/200-bagin023bk.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "【楽天1位受賞】【YKK製ファスナー】【ほつれ穴なし】【撥水加工】 トラベルポーチ 圧縮 YKK セット 圧縮ポーチ 圧縮袋 衣類 旅行 便利グッズ 旅行用 旅行用圧縮袋 トラベル 圧縮バッグ 圧縮バック 収納ポーチ 旅行用ポーチ 服 両面 出張 おむつ",
@@ -30,7 +34,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.72（2,209件）・ポイント10倍・送料無料",
     "price": "1,780円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00tmtxo.82zek2e8.g00tmtxo.82zelc8c/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fvalue-create%2Ftravel-pouch%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fvalue-create%2Fi%2F10000010%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/value-create/cabinet/travelpouch/0328_2.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "期間限定！1,480円〜【楽天年間1位！形状記憶特許品】 日傘 折りたたみ傘 完全遮光 JIS認証済 遮熱率59％ 自動開閉 DeliToo 傘 超軽量 わずか240g 折り畳み傘 6本骨 uvカット UPF 50+ 紫外線対策 日焼け対策 メンズ レディース 軽量 晴雨 花粉対策 ギフト",
@@ -38,7 +44,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.31（46,031件）・送料無料",
     "price": "2,980円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00tyy9o.82zek4c8.g00tyy9o.82zel8cd/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fazusa%2Fb16gcq%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fazusa%2Fi%2F10000109%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/azusa/cabinet/6san/imgrc0171182741.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "【スーパーSALE 50%OFFクーポン対象】送料無料 アヴェンチュラ リュック リュックサック デイパック 撥水 ナイロン 大容量 a4 ファスナー シューズポケット バックパック 19L 黒 二層式 女子 男子 男女兼用 高校生 レディース 女性 大人 習い事 かわいい 可愛い おしゃれ",
@@ -46,7 +54,9 @@ window.PRODUCTS = [
     "description": "レビュー★5（3件）・送料無料",
     "price": "4,950円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00rlido.82zekeb5.g00rlido.82zela3b/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fdixel%2F30502%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fdixel%2Fi%2F10001783%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/dixel/cabinet/rucksack01/30502.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "【楽天1位受賞】ガジェットポーチ 小物入れ 5ポケット ストラップ付 旅行 海外旅行 出張 収納 整理 トラベルポーチ 内寸18.5cm 奥行13cm",
@@ -54,7 +64,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.48（157件）・送料無料",
     "price": "1,580円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00pmmbo.82zek906.g00pmmbo.82zela6b/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fsanwadirect%2F200-bagin005%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fsanwadirect%2Fi%2F10087325%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/sanwadirect/cabinet/2/200-bagin005.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "楽天ランキング1位＼81%OFF＆P2倍で2,147円！／2024年間MVP ワイヤレスイヤホン bluetoothイヤホン 残量表示 Bluetooth6.0 ENC ノイズキャンセリング 自動ペアリング 36H連続再生 Type‐C急速充電 IPX7防水 AAC対応 マイク内蔵 iPhone/Android 旅行",
@@ -62,7 +74,9 @@ window.PRODUCTS = [
     "description": "レビュー★4.51（49,624件）・送料無料",
     "price": "2,580円",
     "url": "https://hb.afl.rakuten.co.jp/hgc/g00u931o.82zekc8a.g00u931o.82zel165/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fgracevally%2Fgrace-x1%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fgracevally%2Fi%2F10000003%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
-    "expires_on": ""
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/gracevally/cabinet/09355687/09355695/12855263/x1-2026front-60.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   },
   {
     "name": "ダブルポイント祭り　7000円以上でポイント２倍　今日まで",
@@ -70,6 +84,28 @@ window.PRODUCTS = [
     "description": "気になった商品をチェック。",
     "price": "",
     "url": "https://link.amazon/B017dkmDl",
-    "expires_on": "2026-09-13"
+    "expires_on": "2026-09-13",
+    "image_url": "",
+    "added_on": "2026-09-13"
+  },
+  {
+    "name": "【14日まで限定5%OFFクーポン】 タオルハンガー＆補助テーブル | タオルハンガー 補助テーブル 折りたたみ キッチン 狭い 台所 作業台 工夫 送料無料",
+    "category": "楽天市場",
+    "description": "レビュー★4.27（11件）・送料無料",
+    "price": "3,647円",
+    "url": "https://hb.afl.rakuten.co.jp/hgc/g00ri5fo.82zekae8.g00ri5fo.82zel435/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ftrendstyle%2Fsu_t_hunge%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Ftrendstyle%2Fi%2F10004909%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/trendstyle/cabinet/hk/rs/su_t_hunge.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
+  },
+  {
+    "name": "【助産師さん監修×ベビモこどもに掲載】＼楽天1位／ おむつストッカー オムツストッカー 収納バッグ ベビー用品 おしりふき おむつ ケース 新生児 赤ちゃん ふた付き 大容量 ポケット付き 出産祝い ギフト プレジュール(Plaisiureux)",
+    "category": "楽天市場",
+    "description": "レビュー★4.4（1,019件）・送料無料",
+    "price": "2,480円",
+    "url": "https://hb.afl.rakuten.co.jp/hgc/g00tb0po.82zek93d.g00tb0po.82zel1ce/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fplaisiureux%2Fpl_36%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fplaisiureux%2Fi%2F10000063%2F&rafcid=wsc_i_is_ccea80f3-e059-47ab-a7c8-f1cc79eda553",
+    "expires_on": "",
+    "image_url": "https://thumbnail.image.rakuten.co.jp/@0_mall/plaisiureux/cabinet/09353293/10451160/imgrc0161294108.jpg?_ex=128x128",
+    "added_on": "2026-09-13"
   }
 ];
